aad9e245d0866d28f2600c2e5a11e088ffbf6042
zlfyuan <741136856@qq.com>
2021-03-26 16:44:41 +0800

    修改验证文件

--- Example/Pods/Target Support Files/Pods-AFNetworkingPlugin_Tests/Pods-AFNetworkingPlugin_Tests-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-AFNetworkingPlugin_Tests/Pods-AFNetworkingPlugin_Tests-frameworks.sh
@@ -175,16 +175,10 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Expecta/Expecta.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Expecta+Snapshots/Expecta_Snapshots.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/FBSnapshotTestCase/FBSnapshotTestCase.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Specta/Specta.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ZLFBaseConfigTool/ZLFBaseConfigTool.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Expecta/Expecta.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Expecta+Snapshots/Expecta_Snapshots.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/FBSnapshotTestCase/FBSnapshotTestCase.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Specta/Specta.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ZLFBaseConfigTool/ZLFBaseConfigTool.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
